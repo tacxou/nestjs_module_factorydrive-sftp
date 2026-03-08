b3195171332945c8b19ac2a04d3d45305185f1b2
Tacx <12997062+tacxou@users.noreply.github.com>
2026-03-08 20:32:23 +0100

    update import path for SFTP storage module to reflect new package namespace

--- src/sftp.storage.ts
+++ src/sftp.storage.ts
@@ -10,7 +10,7 @@ import {
   Response,
   StatResponse,
   UnknownException,
-} from '@the-software-compagny/nestjs_module_factorydrive'
+} from '@tacxou/nestjs_module_factorydrive'
 import { PassThrough } from 'node:stream'
 import Client, { ConnectOptions } from 'ssh2-sftp-client'
 
